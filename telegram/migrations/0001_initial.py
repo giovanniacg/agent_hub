156de95d48c3bb0f4ee735377980fe074375ecf5
Giovanni Giampauli <giovanni.acg@gmail.com>
2025-11-02 23:23:47 -0300

    ruff format

--- telegram/migrations/0001_initial.py
+++ telegram/migrations/0001_initial.py
@@ -4,27 +4,33 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='TelegramUser',
+            name="TelegramUser",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('telegram_id', models.BigIntegerField(unique=True)),
-                ('username', models.CharField(blank=True, max_length=150, null=True)),
-                ('first_name', models.CharField(blank=True, max_length=150, null=True)),
-                ('last_name', models.CharField(blank=True, max_length=150, null=True)),
-                ('is_admin', models.BooleanField(default=False)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                ("telegram_id", models.BigIntegerField(unique=True)),
+                ("username", models.CharField(blank=True, max_length=150, null=True)),
+                ("first_name", models.CharField(blank=True, max_length=150, null=True)),
+                ("last_name", models.CharField(blank=True, max_length=150, null=True)),
+                ("is_admin", models.BooleanField(default=False)),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
     ]
